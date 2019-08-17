876c06de567377732dba72b34d651541a0313151
JinFree <ytrqwe12@gmail.com>
2019-08-17 16:48:07 +0900

    test_for_yolov3

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p build && cd build && cmake -DNCNN_VULKAN=OFF .. && make -j2
+## DOWNLOAD
+#param : https://drive.google.com/open?id=1V9oKHP6G6XvXZqhZbzNKL6FI_clRWdC-
+#bin : https://drive.google.com/open?id=1DBcuFCr-856z3FRQznWL_S5h-Aj3RawA
+
